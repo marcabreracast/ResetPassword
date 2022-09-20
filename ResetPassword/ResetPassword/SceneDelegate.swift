15f1a9b1350aaf3af4467e0cdd8c68420301546c
Mar Cabrera <mar.cabrera@mongodb.com>
2022-09-20 13:40:59 +0100

    Add Reset Password functionality and basic layouts

--- ResetPassword/ResetPassword/SceneDelegate.swift
+++ ResetPassword/ResetPassword/SceneDelegate.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import RealmSwift
+
+let app = App(id: "resetpassword-cngnr")
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -17,6 +20,51 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        // Universal Links Handling
+        
+        guard let userActivity = connectionOptions.userActivities.first, userActivity.activityType == NSUserActivityTypeBrowsingWeb,
+                let incomingURL = userActivity.webpageURL else {
+            // If we don't get a link (meaning it's not handling the reset password flow then we have to check if user is logged in)
+            if let _ = app.currentUser {
+                // We make sure that the session is being kept active for users that have previously logged in
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let mainVC = storyboard.instantiateViewController(identifier: "MainViewController")
+
+                window?.rootViewController = mainVC
+                window?.makeKeyAndVisible()
+            }
+            return
+        }
+
+        handleUniversalLinks(incomingURL)
+    }
+    
+    /**
+     This delegate method gets triggered whenever user taps on link from email and app has not been terminated
+     */
+    func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
+
+        if let url = userActivity.webpageURL {
+            handleUniversalLinks(url)
+        }
+    }
+
+    
+    private func handleUniversalLinks(_ url: URL) {
+        // We get the token and tokenId URL parameters, they're necessary in order to reset password
+        let token = url.valueOf("token")
+        let tokenId = url.valueOf("tokenId")
+
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let resetPasswordViewController = storyboard.instantiateViewController(identifier: "ResetPasswordViewController") as! ResetPasswordViewController
+        // We might not need a navigation controller
+        let navigationController = UINavigationController(rootViewController: resetPasswordViewController)
+        resetPasswordViewController.token = token
+        resetPasswordViewController.tokenId = tokenId
+
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -46,7 +94,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
+}
 
+extension URL {
+    // Function that returns a specific query parameter from the URL
+    func valueOf(_ queryParameterName: String) -> String? {
+        guard let url = URLComponents(string: self.absoluteString) else { return nil }
 
+        return url.queryItems?.first(where: {$0.name == queryParameterName})?.value
+    }
 }
 
